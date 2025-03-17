58a7cfe4ab26cd1001b43b65376c326083b1912a
danielmeyer0101 <danielmeyer0101@gmail.com>
2025-03-17 22:25:33 +0100

    Bestillings kode

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("press 1 to show menu");
-        Menu.showmenu();
+        Bestilling ordre1 = new Bestilling("2x nr.3 + 1 x nr.17", 15);
+
+        Bestilling nyBestillingsListe = new Bestilling();
+
+        nyBestillingsListe.tilføjBestilling(ordre1);
+
+        nyBestillingsListe.visBestillinger();
+
+
+
     }
 }
