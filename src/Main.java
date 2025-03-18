94b12b02b05bcedd8fa4609c88075215df875186
RasmusLundbergh <Rasmus.lundbergh@gmail.com>
2025-03-18 13:02:17 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("press 1 to show menu");
+        System.out.println(" ___________________________________");
+        System.out.println("|               MENU                |");
+        System.out.println(" ___________________________________");
+        System.out.println("|  Press 1 for Pizza overview       |");
+        System.out.println("|  Press 2 to create a order        |");
+        System.out.println("|  Press 3 to see order overview    |");
+        System.out.println("|  Press 4 to see completed orders  |");
+        System.out.println("|  Press 5 to see statistics        |");
+        System.out.println(" ___________________________________");
+
         Menu.showmenu();
     }
 }
