617ae1833714630a7f1e5a2b0b68164510e35430
RasmusLundbergh <Rasmus.lundbergh@gmail.com>
2025-03-19 09:36:30 +0100

    Sammensætning af alles kode

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class Bestilling {
+
+    String pizza;
+    int afhentningstid;
+    ArrayList<Bestilling> bestillingsListe;
+
+    public Bestilling() {
+        bestillingsListe = new ArrayList<>();
+    }
+
+    public Bestilling (String pizza,int afhentningstid) {
+        this.pizza = pizza;
+        this.afhentningstid = afhentningstid;
+    }
+
+    public void tilføjBestilling(Bestilling b) {
+        bestillingsListe.add(b);
+    }
+
+    public void visBestilling() {
+        System.out.println("Bestilling: " + this.pizza + ", Afhentningstid: " + this.afhentningstid + " min");
+    }
+
+    public void visBestillinger() {
+        for (int i = 0; i < bestillingsListe.size(); i++) {
+            bestillingsListe.get(i).visBestilling();
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,56 @@
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" ___________________________________");
-        System.out.println("|               MENU                |");
-        System.out.println(" ___________________________________");
-        System.out.println("|  Press 1 for Pizza overview       |");
-        System.out.println("|  Press 2 to create a order        |");
-        System.out.println("|  Press 3 to see order overview    |");
-        System.out.println("|  Press 4 to see completed orders  |");
-        System.out.println("|  Press 5 to see statistics        |");
-        System.out.println(" ___________________________________");
+        Bestilling nyBestillingsListe = new Bestilling();
 
-        Menu.showmenu();
+        boolean programmetSkalKøre = true;
+
+        while (programmetSkalKøre) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(" ___________________________________");
+            System.out.println("|               MENU                |");
+            System.out.println(" ___________________________________");
+            System.out.println("|  Press 1 for Pizza overview       |");
+            System.out.println("|  Press 2 to create a order        |");
+            System.out.println("|  Press 3 to see order overview    |");
+            System.out.println("|  Press 4 to see completed orders  |");
+            System.out.println("|  Press 5 to see statistics        |");
+            System.out.println(" ___________________________________");
+
+            int brugerValg = scanner.nextInt();
+            if (brugerValg == 1){
+                Menu.showmenu();
+            }
+            else if (brugerValg == 2) {
+
+                scanner.nextLine();
+
+                System.out.println("Indtast bestilling");
+                String inputBestilling = scanner.nextLine();
+
+                System.out.println("Indtast afhentningstid");
+                int inputAfhentningstidspunkt = scanner.nextInt();
+
+                Bestilling ordre = new Bestilling(inputBestilling, inputAfhentningstidspunkt);
+
+                nyBestillingsListe.tilføjBestilling(ordre);
+
+            }
+
+            else if (brugerValg == 3) {
+                nyBestillingsListe.visBestillinger();
+            }
+
+            else if (brugerValg == 6) {
+                programmetSkalKøre = false;
+            }
+        }
+        System.out.println("Programmet er afsluttet");
     }
 }
