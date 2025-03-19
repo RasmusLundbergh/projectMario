dd61a9a1c517bc1867d47ce0f1b623dd803dd8a3
danielmeyer0101 <danielmeyer0101@gmail.com>
2025-03-19 04:46:40 +0100

    Videreudvikling

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,53 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Bestilling ordre1 = new Bestilling("2x nr.3 + 1 x nr.17", 15);
-
         Bestilling nyBestillingsListe = new Bestilling();
 
-        nyBestillingsListe.tilføjBestilling(ordre1);
+        boolean programmetSkalKøre = true;
 
-        nyBestillingsListe.visBestillinger();
+        while (programmetSkalKøre) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Menu:");
+            System.out.println("1: Vis menu ");
+            System.out.println("2: Opret ordre/bestilling ");
+            System.out.println("3: Vis ordreoversigt/bestillingsliste ");
+            System.out.println("4:... ");
+            System.out.println("5:...");
+            System.out.println("6: Afslut programmet");
+            System.out.println("Vælg funktion:");
 
+            int brugerValg = scanner.nextInt();
 
+            if (brugerValg == 2) {
 
+                scanner.nextLine();
+
+                System.out.println("Indtast bestilling");
+                String inputBestilling = scanner.nextLine();
+
+                System.out.println("Indtast afhentningstid");
+                int inputAfhentningstidspunkt = scanner.nextInt();
+
+                Bestilling ordre = new Bestilling(inputBestilling, inputAfhentningstidspunkt);
+
+                nyBestillingsListe.tilføjBestilling(ordre);
+
+            }
+
+            else if (brugerValg == 3) {
+                nyBestillingsListe.visBestillinger();
+            }
+
+            else if (brugerValg == 6) {
+                programmetSkalKøre = false;
+            }
+        }
+        System.out.println("Programmet er afsluttet");
     }
 }
